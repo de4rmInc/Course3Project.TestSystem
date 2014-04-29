b7040a8374c266e3640ff21c8c5fdfc883372eda
de4rmInc <perovskijab@gmail.com>
2014-04-30 00:50:02 +0300

    implemented different behavior for different tests

--- Course3Project/TestSystem/TestSystem.py
+++ Course3Project/TestSystem/TestSystem.py
@@ -2,18 +2,19 @@ import exercise_one as ex1
 import io
 import ast
 import re
+import inspect
 
-predefined_vars = ['e1','e2','e3','e4','e5','e6']
+predefined_vars = ['e1','e2','e3','e4','e5','e6','e7','e8']
 
 
 testvars = vars(ex1).copy()
 module_lines = list(open('exercise_one.py'))
 
-def find_lines(lines,pattern=''):
+def find_lines(lines, pattern=''):
     strings = list(filter(lambda l: pattern in l,lines))
     return strings
 
-def find_line(lines,pattern=''):
+def find_line(lines, pattern=''):
     elem = list(filter(lambda l:pattern in l,lines))
     return elem[0] if len(elem) else None
 
@@ -25,6 +26,17 @@ def print_dump(line):
     dump = ast.dump(ast.parse(line))
     print("line: %sdump: %s\n" % (line,dump))
 
+def isFunction(obj):
+    return hasattr(obj, '__call__')
+
+def verify_function(args, func1, func2):
+    equivalent = True
+    for arg in args:
+        result1 = func1(arg)
+        result2 = func2(arg)
+        equivalent = equivalent and result1 == result2
+        if not equivalent: return equivalent
+    return equivalent
 a = 0
 
 def test_variables(variables):
@@ -35,37 +47,57 @@ def test_variables(variables):
     for x in [1,2,3]:
         print(variables['e7'](x))
 
-def run_tests(tests={},variables=[]):
+def run_tests(tests={}):
+    variables = sorted(tests.keys())
     for v in variables:
-        testfuncvars= predefined_tests[v][0]
-        testfunc = predefined_tests[v][1]
+        print('tests for variable %s:' % v)
+        good_job = True
+        testvar = testvars[v]
+        for test in tests[v]:            
+            testfuncargs = test[0]
+            testfunc = test[1]
+            
+            if(testfunc != None and len(testfuncargs)):
+                if(isFunction(testvar)):
+                    good_job = good_job and verify_function(testfuncargs,testvar,testfunc)
+                elif testfunc.__name__ == 'find_line':
+                    for funcarg in testfuncargs:
+                        print('%s(\'%s\')\t\t' % (testfunc.__name__,funcarg))
+                        good_job = good_job and testfunc(find_lines(module_lines,v),funcarg) != None
+                elif len(inspect.getfullargspec(testfunc)[0]) == 2:
+                    for funcarg in testfuncargs:
+                        good_job = good_job and testfunc(funcarg,testvar)
+                else:
+                    good_job = good_job and testfunc(testvar)
+                
+        print('All tests completed successfully.' if good_job else 'Try next time, please. Fix errors.')
+                        
+###
+#START>>>Section for predefined test functions
+###
+def e1_testfunction(obj):
+    return obj > 10
 
-        print('tests for variable %s:'%v)
+def e3_testfunction(o1,o2):
+    return True if o1 == o2 else None
 
-        if(testfunc != None and len(testfuncvars)):
-            for funcvar in testfuncvars:
-                print('%s(\'%s\')\t\t'%(testfunc.__name__,funcvar))
-                print(testfunc(find_lines(module_lines,v),funcvar) != None)
+def e5_testfunction(o1):
+    return type(o1) is tuple
 
-test_variables(testvars)
-print(find_lines(module_lines))
-
-print_dumps(find_lines(module_lines))
-
-
-
+def e7_testfunction(a):
+    return a * a
+###
+#END>>>Section for predefined test functions
+###
 predefined_tests = \
-    {'e1':(['%'],find_line),
-     'e2':(['str','ing','ingstr'],find_line),
-     'e3':([],None),
-     'e4':([],None),
-     'e5':([],None),
-     'e6':([],None)}
+    {'e1':[(['%','/'],find_line), ([0],e1_testfunction)],
+     'e2':[(['ingstr','ing','ingstr'],find_line)],
+     'e3':[([[1,2,3,4,5]],e3_testfunction)],
+     'e4':[([],None)],
+     'e5':[([0],e5_testfunction), (['e1','e2'],find_line)],
+     'e6':[(['math','log'],find_line)],
+     'e7':[([1,2,3,4],e7_testfunction)],
+     'e8':[([],None)]
+     }
 
-run_tests(predefined_tests,predefined_vars)
-
-#testvars_temp = [x for x in predefined_tests['e1'][0]]
-#testfunc = predefined_tests['e1'][1]
-
-#if(testfunc != None and len(testvars_temp)):
-#    print(testfunc(find_lines(module_lines,'e1'),testvars_temp[0]) != None)
+run_tests(predefined_tests)
