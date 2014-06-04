0313529c1bbebd333cc31506d507648ce08c862c
Aliaksei Piarouski <aliaksei_piarouski@epam.com>
2014-06-04 12:17:18 +0300

    web app was made

---
+++ Course3Project/WebDjangoApp_C3P/WebDjangoApp_C3P/C3P_WebApp/views.py
@@ -0,0 +1,5 @@
+from django.http import HttpResponse
+from django.template.loader import render_to_string
+
+def home(request):
+    return HttpResponse(render_to_string('index.html', {'content':'May the force be with you! (c) Master Yoda'}))
